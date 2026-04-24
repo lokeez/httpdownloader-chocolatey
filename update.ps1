85a58f5f76fd3d4c5a2f21b4a019698698f84c3a
lokeez <stadnyk.oleg@gmail.com>
2026-04-24 21:36:57 +0300

    Create update.ps1

---
+++ update.ps1
@@ -0,0 +1,29 @@
+Import-Module au
+
+$repo = 'erickutcher/httpdownloader'
+
+function global:au_GetLatest {
+    $release = Invoke-RestMethod "https://api.github.com/repos/$repo/releases/latest"
+
+    $version = $release.tag_name.TrimStart('v')
+
+    $url32 = $release.assets | Where-Object { $_.name -match '32\.zip$' } | Select-Object -ExpandProperty browser_download_url
+    $url64 = $release.assets | Where-Object { $_.name -match '64\.zip$' } | Select-Object -ExpandProperty browser_download_url
+
+    return @{
+        Version = $version
+        URL32   = $url32
+        URL64   = $url64
+    }
+}
+
+function global:au_SearchReplace {
+    @{
+        ".\tools\chocolateyinstall.ps1" = @{
+            "(?i)(^\s*url\s*=\s*)'.*'"      = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*url64bit\s*=\s*)'.*'" = "`$1'$($Latest.URL64)'"
+        }
+    }
+}
+
+update -ChecksumFor 32,64
