465cb8b6a4b08cdc1c8af600247996c40ab485de
lokeez <stadnyk.oleg@gmail.com>
2026-04-26 08:54:58 +0300

    Delete httpdownloader/tools/verifier.ps1

--- httpdownloader/tools/verifier.ps1
+++
@@ -1,9 +0,0 @@
-$toolsDir = Join-Path $env:ChocolateyInstall "lib\httpdownloader\tools"
-
-$exe = Get-ChildItem $toolsDir -Recurse -Filter "HTTP_Downloader.exe" | Select-Object -First 1
-
-if (-not $exe) {
-    throw "Executable not found in package"
-}
-
-Write-Host "Found executable at: $($exe.FullName)"
